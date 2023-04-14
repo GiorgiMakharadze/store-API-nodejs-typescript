845c72389853f403783ddec798ae658c28baeea4
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-04-14 10:20:47 +0400

    express server&middleware

--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
-console.log("Store API");
+import "dotenv/config";
+import express from "express";
+import { errorHandlerMiddleware } from "./api/middleware/error-handler";
+import { notFound } from "./api/middleware/not-found";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store api</h1><a href="/api/v1/products">products route</a>');
+});
+
+//prodcuts route
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    //connectDb
+    app.listen(port, () => {
+      `Server is listening port ${port}`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
